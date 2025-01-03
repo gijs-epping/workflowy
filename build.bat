17dce62d57209f4b9a1702f0c408130343754308
gijs-epping <gijs.epping@strix.net>
2025-01-03 06:39:28 +0100

    Update build, manifest and refactor info

--- build.bat
+++ build.bat
@@ -3,8 +3,8 @@ echo Creating zip file for Chrome extension...
 
 if exist workflowy-extension.zip del workflowy-extension.zip
 
-powershell -command "Compress-Archive -Path manifest.json, background, src, content, popup, icons -DestinationPath workflowy-extension.zip -Force"
+powershell -command "Compress-Archive -Path manifest.json, background, src, popup, icons -DestinationPath workflowy-master.zip -Force"
 
 echo Zip file created successfully!
-echo Location: workflowy-extension.zip
+echo Location: workflowy-master.zip
 pause
